f8c8b0f2631f736d49ede58df5d1a7b2e0b72826
Kaan İzgi <kaanizgi1531@gmail.com>
2022-03-29 17:07:43 +0300

    Initial Commit

---
+++ Crypto-SwiftUI/Crypto_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_SwiftUIApp.swift
+//  Crypto-SwiftUI
+//
+//  Created by Kaan İzgi on 29.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
