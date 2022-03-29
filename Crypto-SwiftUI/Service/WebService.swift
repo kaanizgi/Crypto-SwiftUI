ad5e1c83f5397ed170382edada80ff9661f024e7
Kaan İzgi <kaanizgi1531@gmail.com>
2022-03-29 18:29:16 +0300

    Refactoring
    Webservice refactoring

--- Crypto-SwiftUI/Service/WebService.swift
+++ Crypto-SwiftUI/Service/WebService.swift
@@ -12,6 +12,7 @@ class WebService {
     
     func downloadCurrencies(url:URL) async throws -> [CryptoCurrency] {
         let (data,response) = try await URLSession.shared.data(from: url)
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Error while fetching data") }
         let currencies = try? JSONDecoder().decode([CryptoCurrency].self, from: data)
         return currencies ?? []
     }
